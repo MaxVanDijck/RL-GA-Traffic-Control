840e2256caee0797e6dcdc238bbe23c49ab007fe
Frostyyxo <vandijcksm@gmail.com>
2021-08-11 11:18:42 +1200

    population

--- ga.py
+++ ga.py
@@ -1,14 +1,19 @@
 import gym_cityflow
 import gym
+import random
 
-#Initialize Environment and check action space
+#Initialize Environment
 env = gym.make('gym_cityflow:cityflow-v0', 
-                configPath = '')
+                configPath = 'data/1x1_config.json',
+                episodeSteps = 3600)
 
 #initialise Population:
 #parameters:
 popSize = 100
-
-population = []
+population = {}
 for i in range(popSize):
     solution = []
+    for j in range(12):
+        solution.append(random.randint(0, 8))
+
+    population['solution' + str(i)] = solution
