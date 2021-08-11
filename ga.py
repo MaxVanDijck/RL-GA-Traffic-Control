408c4fdb5815a959508c4f53defbccd3c729e8aa
Frostyyxo <vandijcksm@gmail.com>
2021-08-11 15:37:02 +1200

    Update ga.py

--- ga.py
+++ ga.py
@@ -4,16 +4,45 @@ import random
 
 #Initialize Environment
 env = gym.make('gym_cityflow:cityflow-v0', 
-                configPath = 'data/1x1_config.json',
+                configPath = 'data/4x4_config.json',
                 episodeSteps = 3600)
 
 #initialise Population:
-#parameters:
-popSize = 100
-population = {}
-for i in range(popSize):
-    solution = []
-    for j in range(12):
-        solution.append(random.randint(0, 8))
+def createPopulation(popSize):
+    population = {}
+    for i in range(popSize):
+        solution = []
+        for j in range(12):
+            singleActionStep = []
+            for k in range(len(env.action_space.nvec)):
+                singleActionStep.append(random.randint(0, env.action_space.nvec[k]-1))
+            solution.append(singleActionStep)
+        population['solution' + str(i)] = solution
+    return population
 
-    population['solution' + str(i)] = solution
+population = createPopulation(popSize=100)
+
+#Iterate through population and get fitness
+popFitness = {}
+for key, val in population.items():
+    done = False
+    count = 0
+    cumulativeReward = 0
+
+    stepCounter = 0
+    while done == False:
+        print(stepCounter)
+        stepCounter +=1
+        stepCounter = stepCounter % 10
+        if stepCounter == 0:
+            count += 1
+            count = count % 11
+        observation, reward, done, debug = env.step(population[key][count])
+        for arr in reward:
+            for i in range(len(arr)):
+                if i != 0:
+                    cumulativeReward += arr[i]
+
+    popFitness[key] = cumulativeReward
+    print(key + 'Finished, reward:' + str(reward))
+    break
